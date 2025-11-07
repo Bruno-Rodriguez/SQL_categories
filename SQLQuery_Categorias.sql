d4c4d47d3c8e574507a7715fa1b98af076176869
Bruno Rodriguez <50221004+Bruno-Rodriguez@users.noreply.github.com>
2025-11-07 10:18:38 -0500

    Corrected characters with accents

--- SQLQuery_Categorias.sql
+++ SQLQuery_Categorias.sql
@@ -1,4 +1,4 @@
--- Crear vista con IDs y nombres de conductores, n�mero de viajes completados, fracci�n del total de viaje, acumulado de las fracciones, y la categor�a correspondiente
+-- Crear vista con IDs y nombres de conductores, número de viajes completados, fracción del total de viaje, acumulado de las fracciones, y la categoría correspondiente
 CREATE VIEW conductores_categorias AS 
 WITH trip_counts AS (
 	SELECT 
@@ -24,11 +24,11 @@ WITH trip_counts AS (
 SELECT DriverID AS 'ID Conductor', 
 		CONCAT(TRIM(dp.[driver_profile.first_name]),' ',CONCAT(TRIM(dp.[driver_profile.middle_name]),' ',TRIM(dp.[driver_profile.last_name]))) AS 'Nombre Conductor',
 		Trips AS 'N. Viajes',
-		ROUND(Trips_f,4) AS 'Fracci�n de total de viajes',
-		ROUND(Trips_f_acc,4) AS 'Fracci�n acumulada de total de viajes',
-		CASE WHEN (Trips_f_acc > 0.9) THEN 'Categor�a 1'
-			 WHEN (Trips_f_acc BETWEEN 0.7 AND 0.9) THEN 'Categor�a 2'
-			 WHEN (Trips_f_acc BETWEEN 0.4 AND 0.7) THEN 'Categor�a 3' ELSE 'Categor�a 4' END AS 'Categor�a'
+		ROUND(Trips_f,4) AS 'Fracción de total de viajes',
+		ROUND(Trips_f_acc,4) AS 'Fracción acumulada de total de viajes',
+		CASE WHEN (Trips_f_acc > 0.9) THEN 'Categoría 1'
+			 WHEN (Trips_f_acc BETWEEN 0.7 AND 0.9) THEN 'Categoría 2'
+			 WHEN (Trips_f_acc BETWEEN 0.4 AND 0.7) THEN 'Categoría 3' ELSE 'Categoría 4' END AS 'Categoría'
 FROM totals t
   LEFT JOIN [Database].[dbo].[DriverProfiles] dp
   ON t.DriverID = dp.[driver_profile.id];
@@ -40,8 +40,9 @@ FROM conductores_categorias
 ORDER BY 'N. Viajes' DESC;
 
 
--- Visualizar la cuenta de conductores por cada categor�a
-SELECT Categor�a, COUNT(*) AS 'Conductores por categor�a'
+-- Visualizar la cuenta de conductores por cada categoría
+SELECT Categoría, COUNT(*) AS 'Conductores por categoría'
 FROM conductores_categorias
-GROUP BY Categor�a
-ORDER BY 'Conductores por categor�a' ASC;
+GROUP BY Categoría
+
+ORDER BY 'Conductores por categoría' ASC;
